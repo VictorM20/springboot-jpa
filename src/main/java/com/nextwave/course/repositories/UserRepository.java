9edd72dcead28df3094a52decf4603d4edc97e2b
Victor Mendes <vhs.mendes18@gmail.com>
2023-07-26 20:26:41 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/nextwave/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nextwave.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nextwave.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+}
