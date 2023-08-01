270402903383bb540f1cbd860de6cbffcbaee5d3
Victor Mendes <vhs.mendes18@gmail.com>
2023-07-31 21:17:03 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/nextwave/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.nextwave.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nextwave.course.entities.Order;
+import com.nextwave.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
