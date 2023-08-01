c606098c86aa183e8d5028ac80c0f2e3c6692349
Victor Mendes <vhs.mendes18@gmail.com>
2023-08-01 20:43:05 -0300

    Product

---
+++ src/main/java/com/nextwave/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.nextwave.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nextwave.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product,Long>{
+	
+}
