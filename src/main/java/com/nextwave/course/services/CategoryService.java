adccf726ad5dae39dd57fa1bb7c440027f42411b
Victor Mendes <vhs.mendes18@gmail.com>
2023-08-01 20:26:10 -0300

    Category

---
+++ src/main/java/com/nextwave/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.nextwave.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nextwave.course.entities.Category;
+import com.nextwave.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
