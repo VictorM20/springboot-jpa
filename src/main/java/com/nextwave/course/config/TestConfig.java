8fad88be91639c7abfe279bcea581e2f4b3b8464
Victor Mendes <vhs.mendes18@gmail.com>
2023-08-02 20:20:28 -0300

    Payment, one-to-one association

--- src/main/java/com/nextwave/course/config/TestConfig.java
+++ src/main/java/com/nextwave/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nextwave.course.entities.Category;
 import com.nextwave.course.entities.Order;
 import com.nextwave.course.entities.OrderItem;
+import com.nextwave.course.entities.Payment;
 import com.nextwave.course.entities.Product;
 import com.nextwave.course.entities.User;
 import com.nextwave.course.entities.enums.OrderStatus;
@@ -69,7 +70,7 @@ public class TestConfig implements CommandLineRunner {
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u1);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
